5c2475f25358871655adaf282616b65d6b999407
Wang Baojun <catandmore@users.noreply.github.com>
2017-04-09 00:13:44 +0800

    子弹飞行脚本
    让子弹飞

---
+++ tank1/fly.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fly : MonoBehaviour {
+    Transform _fly;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        _fly = GetComponent<Transform>();
+        speed = 500f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _fly.Translate(0, 0, -speed * Time.deltaTime);
+   
+    }
+
+}
