2166a03ebd2c2fa8319b81b4d23cffd9d4411324
Wang Baojun <catandmore@users.noreply.github.com>
2017-04-09 00:16:03 +0800

    子弹撞击脚本
    子弹撞击后自毁并产生粒子特效

---
+++ tank1/hit_bullet.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hit_bullet : MonoBehaviour {
+    GameObject clone;
+    GameObject Instcube;
+    public int a=0;
+
+	// Use this for initialization
+	void Start () {
+        Instcube = GameObject.Find("Particle System");
+	}
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        a = 1;
+    }
+   
+
+    // Update is called once per frame
+	void Update () {
+        if(a==1)
+        {
+            clone = GameObject.Instantiate(Instcube, gameObject.transform.position, gameObject.transform.rotation);
+            Destroy(this.gameObject);
+        }
+        Destroy(clone, 1);
+	}
+}
