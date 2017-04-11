fa31d393d449b9c20be957e8aa5b7cf9fc7dd00f
Wang Baojun <catandmore@users.noreply.github.com>
2017-04-11 21:59:54 +0800

    侧翼装甲撞击脚本
    检测侧翼装甲撞击，_time（public）次撞击后，销毁侧翼装甲

---
+++ tank_BATE/hit_armor_LR1.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hit_armor_LR1 : MonoBehaviour {
+    public int _time = 3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void OnCollisionEnter(Collision collision)
+    {
+        _time--;
+    }
+    // Update is called once per frame
+    void Update () {
+        if(_time == 0)
+            Destroy(this.gameObject);
+    }
+}
